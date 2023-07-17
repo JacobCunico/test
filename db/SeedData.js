ad333339761a01f836b14726f49dc58146a8afb1
JacobCunico <Jacob.Cownico@gmail.com>
2023-07-17 17:23:01 -0600

    Added quotes to db

--- db/SeedData.js
+++ db/SeedData.js
@@ -15,25 +15,25 @@ async function createTables() {
     console.log("Starting to build tables");
     await client.query(`
       CREATE TABLE users (
-        userId SERIAL PRIMARY KEY,
+        "userId" SERIAL PRIMARY KEY,
         username VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL
       );
       CREATE TABLE products (
-          productId SERIAL PRIMARY KEY,
+          "productId" SERIAL PRIMARY KEY,
           title VARCHAR(255) UNIQUE NOT NULL,
           description TEXT NOT NULL,
           
           price INTEGER
       ); 
       CREATE TABLE shoppingCart(
-        cartId SERIAL PRIMARY KEY,
-        "ownerId" INTEGER REFERENCES users ( userId )
+        "cartId" SERIAL PRIMARY KEY,
+        "ownerId" INTEGER REFERENCES users ( "userId" )
       );
       CREATE TABLE cartItems(
-        cartItemId SERIAL PRIMARY KEY,
-        "shoppingId" INTEGER REFERENCES shoppingCart ( cartId ),
-        "itemId" INTEGER REFERENCES products ( productId ),
+        "cartItemId" SERIAL PRIMARY KEY,
+        "shoppingId" INTEGER REFERENCES shoppingCart ( "cartId" ),
+        "itemId" INTEGER REFERENCES products ( "productId" ),
         quantity INTEGER
     );
     `);
