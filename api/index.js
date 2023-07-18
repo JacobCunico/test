971bc0268c08cab979f2cdefce975f25981f3fcd
JacobCunico <Jacob.Cownico@gmail.com>
2023-07-18 11:41:16 -0600

    test

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const cartRouter = require("./ShoppingCart");
+const productsRouter = require("./Products");
 
 router.use('/', (req, res, next) => {
     console.log('request came in to the api router');
     next();
   });
 
-router.use("/cart", cartRouter)
+router.use("/cart", cartRouter);
+router.use("/products", productsRouter);
 
 module.exports = router
